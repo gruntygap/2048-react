39a92e82ba3c42f5c4ecd5fdb66975d06eca5e06
gruntygap <gruntygap@gmail.com>
2019-04-25 07:11:03 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/2048-react/precache-manifest.7f7b2318f66efe0c2b9a94b1c639e005.js"
+  "/2048-react/precache-manifest.6d5db1a0baf42d66f944da99b6bd001d.js"
 );
 
 workbox.clientsClaim();
